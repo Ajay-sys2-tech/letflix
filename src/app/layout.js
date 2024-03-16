cf791ad808e109870bd89cba1c60f2c98d78582a
Ajay Yadav <ajay_yadav3@persistent.com>
2024-03-16 19:05:50 +0530

    added navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from '../components/Header'
+import Header from '@/components/Header'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       
       <body className={inter.className}>
-      <Header />
+        <Header />
+        <Navbar />
         {children}
       </body>
     </html>
