fa384c4c29efc1937438ee802305f02bbd2d8c43
ajay4985622 <ajay@vitto.money>
2024-03-17 23:23:09 +0530

    fixed issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-const API_URL = process.env.API_URL;
 const API_KEY = process.env.API_KEY;
 import Results from "@/components/Results";
 
 export default async function Home({searchParams}) {
   console.log(searchParams);
   const genre = searchParams.genre || 'fetchTrending';
-  // const res = await fetch(API_URL);
+
   const res = await fetch (
     `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000}}
