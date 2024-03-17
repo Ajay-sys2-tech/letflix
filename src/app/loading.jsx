38202ac700ce9fc7ec09d4c36804d6775224031c
Ajay Yadav <ajay_yadav3@persistent.com>
2024-03-17 13:18:41 +0530

    added the spinner

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div className='flex justify-center m-10'>
+        <img className='h-46' src='spinner.svg' alt='laoding...' />
+    </div>
+  )
+}
