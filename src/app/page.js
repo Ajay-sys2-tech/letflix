2fec6ef801247ea35cc66d2b687273857316d479
ajay4985622 <ajay@vitto.money>
2024-03-17 15:39:09 +0530

    commit before a pull

--- src/app/page.js
+++ src/app/page.js
@@ -9,13 +9,13 @@ export default async function Home({searchParams}) {
 
   const data = await res.json();
   if(!res.ok){
-    throw new Error('Failed to fetch data');
+    throw new Error('Failed to fetch data');    
   }
 
   const results = data.results;
   return (
     <div>
-    <Results />
+      <Results />
     </div>
   )
 }
