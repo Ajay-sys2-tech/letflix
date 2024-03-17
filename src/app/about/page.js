8a62f3fc254a8f338c46c29a13ddd437ebd42706
ajay4985622 <ajay@vitto.money>
2024-03-17 23:16:43 +0530

    fixed issue

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,13 +5,13 @@ export default function Page () {
         <h1 className='text-2xl font-medium text-amber-600'>About</h1>
 
         <p>
-            As an additional incentive for users, as of 2003, users identified as one of the "top 100 contributors" of 
+            As an additional incentive for users, as of 2003, users identified as one of the top 100 contributors of 
             hard data received complimentary free access to IMDbPro for the following calendar year; for 2006 this was 
             increased to the top 150 contributors, and for 2010 to the top 250.
         </p>
         <p>
-            User profile pages show a user's registration date and, optionally, their personal ratings of titles. 
-            Since 2015, "badges" can be added showing a count of contributions. These badges range from total 
+            User profile pages show a user&apos;s registration date and, optionally, their personal ratings of titles. 
+            Since 2015, badges can be added showing a count of contributions. These badges range from total 
             contributions made to independent categories such as photos, trivia, and biographies. If a registered 
             user or visitor is in the entertainment industry and has an IMDb page, they can add photos through IMDbPRO.
         </p>
@@ -19,7 +19,7 @@ export default function Page () {
 
         <p>
             The title and talent pages of IMDb are accessible to all users, but only registered and logged-in users
-            can submit new material and suggest edits to existing entries. Most of the site's data has been provided 
+            can submit new material and suggest edits to existing entries. Most of the site&apos;s data has been provided 
             by these volunteers. Registered users with a proven track record are able to add and make corrections to 
             cast lists, credits, and some other data points. However, the addition and removal of images, and 
             alterations to titles, cast and crew names, character names, and plot summaries are subject to an approval 
